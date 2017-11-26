b20e0cc7b17a0521c64a2c61acfd0f85fab49799
Byă <hungbya@gmail.com>
2017-11-26 17:08:19 +0700

    Fix some uppercase letter replace

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
 'use strict';
 
 const maps = [
-	['kh', 'x'],
-	['c(?!h)', 'k'],
-	['q', 'k'],
-	['tr', 'c'],
-	['ch', 'c'],
-	['d', 'z'],
-	['gi', 'z'],
-	['r', 'z'],
-	['đ', 'd'],
-	['ph', 'f'],
-	['ngh?', 'q'],
-	['gh', 'g'],
-	['th', 'w'],
-	['nh', 'n\'']
+    ['k(h|H)', 'x'],
+    ['c(?!(h|H))|q', 'k'],
+    ['C(?!(h|H))|Q', 'K'],
+    ['t(r|R)|c(h|H)', 'c'],
+    ['T(r|R)|C(h|H)', 'C'],
+    ['d|g(i|I)(?!\\s)(?!$)|r', 'z'],
+    ['D|G(i|I)(?!\\s)(?!$)|R', 'Z'],
+    ['đ', 'd'],
+    ['Đ', 'D'],
+    ['p(h|H)', 'f'],
+    ['P(h|H)', 'F'],
+    ['n(g|G)(h|H)?', 'q'],
+    ['N(g|G)(h|H)?', 'Q'],
+    ['g(h|H)', 'g'],
+    ['G(h|H)', 'G'],
+    ['th', 'w'],
+    ['T(h|H)', 'W'],
+    ['n(h|H)', 'n\''],
+    ['N(h|H)', 'N\'']
 ];
 
-/**
- * Capitalize the first letter in a string.
- * @param {*} string
- */
-const capitalize = string => `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
-
-/**
- * Convert using a single map element.
- * @param {*} input
- * @param {*} map
- */
-const convert = (input, map) => {
-	return input
-		.replace(new RegExp(map[0], 'g'), map[1])
-		.replace(new RegExp(capitalize(map[0]), 'g'), capitalize(map[1]));
-};
-
 module.exports = input => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
 
-	maps.forEach(map => {
-		input = convert(input, map);
-	});
+    if (typeof input !== 'string') {
+        throw new TypeError(`Expected a string, got ${typeof input}`);
+    }
 
-	return input;
+    maps.forEach(map => {
+        input = input.replace(new RegExp(map[0], 'g'), map[1])
+    });
+
+    return input;
 };
